57db4e17d0f672a52ece8ba0122dfc4ca99b0a9d
Aziza Azizova <aziza.azizova01@gmail.com>
2024-06-06 21:30:14 +0500

    optimized

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useLocation  } from "react-router-dom"
 import Header from "./components/Header"
 import { router } from "./router"
 
 function App() {
+  const location = useLocation();
   return (
     <div className="wrapper">
       <Header />
-        <Routes>
+        <Routes location={location} key={location.pathname}>
           {
             router.map((route, i) => (
               <Route key={i} path={route.path} element={route.element} />
